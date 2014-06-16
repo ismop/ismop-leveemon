8ad5b596f9226879798e6747a5557ccf5d971545
Maciej Pawlik <m.pawlik@cyfronet.pl>
2014-06-16 16:28:44 +0200

    Added functions.js

---
+++ functions.js
@@ -0,0 +1,5 @@
+var lmon = require("./functions/LMonFunctions.js")
+
+exports.getLeveeState = lmon.getLeveeState;
+exports.computeThreatLevel = lmon.computeThreatLevel;
+exports.severeEmergencyActions = lmon.severeEmergencyActions;
